56a7f5eae395dca4148a676e13d3012ec963a878
yannick.frischknecht <yannick.frischknecht@basenet.ch>
2019-11-26 15:56:02 +0100

    constructed base of datagrid

--- Eventlauncher.Web/ClientApp/src/app/home/home.component.ts
+++ Eventlauncher.Web/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { MeetingRoom } from '../models/MeetingRoom.model';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  mockdata: MeetingRoom[];
+
+  ngOnInit(): void {
+    let mr = new MeetingRoom();
+    mr.email = 'eventlauncher@gmail.com';
+    mr.name = 'EventlauncherRoom';
+    mr.ip = '192.164.08.56';
+    this.mockdata = [mr];
+  }
 }
---
+++ Eventlauncher.Web/ClientApp/src/app/models/MeetingRoom.model.ts
@@ -0,0 +1,5 @@
+export class MeetingRoom {
+  name: string;
+  ip: string;
+  email: string;
+}
