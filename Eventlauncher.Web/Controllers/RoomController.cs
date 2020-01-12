ba8c5657f2cbedd84276d39c75e333e97970425c
Yannick Frischknecht <yannickf@bluewin.ch>
2020-01-12 13:47:56 +0100

    added missing API-Endpoints to RoomController

--- Eventlauncher.Web/Controllers/RoomController.cs
+++ Eventlauncher.Web/Controllers/RoomController.cs
@@ -1,4 +1,5 @@
 ﻿using Eventlauncher.Web.Data.Repositories.Abstractions;
+using Eventlauncher.Web.Models.ApiModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -8,18 +9,62 @@ namespace Eventlauncher.Web.Controllers
     [Route("api/room")]
     public class RoomController : ControllerBase
     {
-        private readonly IRoomRepository roomRepository;
+        private readonly IRoomRepository _roomRepository;
 
         public RoomController(IRoomRepository roomRepository)
         {
-            this.roomRepository = roomRepository;
+            _roomRepository = roomRepository;
         }
 
         [Route("list")]
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            return Ok(await roomRepository.GetAllAsync());
+            return Ok(await _roomRepository.GetAllAsync());
+        }
+
+        [Route("create")]
+        [HttpPost]
+        public async Task<IActionResult> Create(MeetingRoom room)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            await _roomRepository.CreateAsync(room.ToEntityModel());
+
+            return Ok();
+        }
+
+        [Route("edit/{id:int}")]
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, MeetingRoom room)
+        {
+            if (id != room.Id)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var toUpdate = await _roomRepository.GetByIdAsync(id);
+            if (toUpdate == null)
+                return NotFound();
+
+            await _roomRepository.UpdateAsync(room.ToEntityModel());
+
+            return Ok();
+        }
+
+        [Route("{id:int}")]
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var toDelete = await _roomRepository.GetByIdAsync(id);
+            if (toDelete == null)
+                return NotFound();
+
+            await _roomRepository.DeleteAsync(toDelete);
+
+            return Ok();
         }
     }
 }
